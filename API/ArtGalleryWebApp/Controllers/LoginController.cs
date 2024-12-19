f5dc43a83f930e1db2cccc1a4565523b0b377490
Marko Frančeski <mfrance@racunarstvo.hr>
2024-12-19 15:30:10 +0100

    Dodan login

---
+++ API/ArtGalleryWebApp/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtGalleryWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username, string password)
+        {
+
+            if (username == "admin" && password == "password")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Invalid username or password.");
+            return View();
+        }
+
+    }
+}
